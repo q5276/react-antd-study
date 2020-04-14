423f9d6d5f08af83893973e52c2b313a2365a2ec
李国帅 <oncwnuOQ65QCeGMp1VOPlkAcJ4ao@git.weixin.qq.com>
2020-04-15 00:51:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import TodoList from './components/TodoList'
+import Logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Layout className="layout">
+    <Layout.Header>
+      <div className="App-logo">
+        <img src={Logo} alt="LOGO" />
+      </div>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1">第一章</Menu.Item>
+        <Menu.Item key="2">菜单2</Menu.Item>
+        <Menu.Item key="3">菜单3</Menu.Item>
+      </Menu>
+    </Layout.Header>
+    <Layout.Content style={{padding: '0 50px'}}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>首页</Breadcrumb.Item>
+        <Breadcrumb.Item>第一章</Breadcrumb.Item>
+        <Breadcrumb.Item>TodoList</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">
+        <TodoList></TodoList>
+      </div>
+    </Layout.Content>
+    <Layout.Footer style={{ textAlign: 'center'}}> Tingno Created By Ant</Layout.Footer>
+  </Layout>
+);
 
 export default App;
